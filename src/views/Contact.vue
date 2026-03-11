4644828af79240bdda376183bcb49dcf3ed0588a
kwanjirakkb_dot <kwanjirakkb@gmail.com>
2026-03-10 23:53:07 -0700

    Initial commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,24 @@
+<template lang="">
+    <div class="container">
+       <div class="alert alert-info" role="alert">
+       A simple info alert—check it out!
+       </div>
+
+       <h1>Show Contact</h1>
+       <h3>Kwanjira Kachinnaphak</h3>
+       <p>Welcome to vue</p>
+
+    </div>
+
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+h3{
+   color: #025aac;    
+}
+</style>
